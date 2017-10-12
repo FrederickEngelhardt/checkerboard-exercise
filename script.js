0d47b3e406f22658023a489c1f85e07d96ab612e
feng <feng@bizdev.us>
2017-10-12 10:29:32 -0600

    my gradient

--- script.js
+++ script.js
@@ -2,7 +2,12 @@
 function createTable(){
 console.log('Function has been activated')
   for (var i = 0; i < 81; i++){
-    var randomColors = '#' + Math.random().toString(16).slice(-6);
+    ranColorArray = []
+    for (let x = 0; x < 5; x++){
+      var randomColors = '#' + Math.random().toString(16).slice(-6)
+      ranColorArray.push(randomColors)
+    }
+    console.log(ranColorArray)
     var tile = document.createElement("div");
     var body = document.getElementsByTagName('body')[0];
     document.body.prepend(tile);
@@ -11,14 +16,11 @@ console.log('Function has been activated')
     tile.display = 'float';
     tile.style.float = 'left';
     if(i%2 === 0){
-      // tile.style.background = 'linear-gradient(to top left,'+randomColors+ ',' + randomColors +')';
-      // tile.style.background = randomColors
-      tile.style.backgroundColor = 'black';
+      tile.style.background = `linear-gradient(to top left,${ranColorArray[0]},${ranColorArray[1]},${ranColorArray[2]},${ranColorArray[3]},${ranColorArray[4]})`
     }
     else if (i%2 === 1) {
-      tile.style.backgroundColor = 'red';
-      // tile.style.background = randomColors
-      // tile.style.background = `linear-gradient(to top left,${ran1},${ran2},${ran3},${ran4},${ran5})`
+
+      tile.style.background = `linear-gradient(to top left,${ranColorArray[0]},${ranColorArray[1]},${ranColorArray[2]},${ranColorArray[3]},${ranColorArray[4]})`
     }
   }
 }
